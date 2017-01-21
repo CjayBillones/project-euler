d3091cf07ef89dc9d2566fbc5a230187cc538f72
Cjay <billonesciprian08@gmail.com>
2017-01-21 21:56:13 +0800

    Remove PrimalityTest function and used PrimalityTest header instead

--- C/project-euler-10.c
+++ C/project-euler-10.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-unsigned long long int isPrime(int);
+#include "PrimalityTest.h"
 
 int main(){
   unsigned long long int sum = 0;
@@ -12,25 +11,4 @@ int main(){
   }
   printf("%llu\n", sum);
   return 0;
-}
-
-unsigned long long int isPrime(int n){
-  
-  int i;
-
-  if(n <= 1)
-    return 0;
-  else if(n <= 3)
-    return 1;
-  else if(n%2 == 0 || n%3 == 0)
-    return 0;
-
-  i = 5;
-  while(i*i <= n){
-    if(n%i == 0 || n%(i+2) == 0)
-      return 0;
-    i = i + 6;
-  }
-
-  return 1;
 }
