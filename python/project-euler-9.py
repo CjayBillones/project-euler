d8fad8632a90294a60f1dc41e9586585829ac90d
Cjay <billonesciprian08@gmail.com>
2017-01-23 00:01:57 +0800

    Add python solution for problem 9

---
+++ python/project-euler-9.py
@@ -0,0 +1,11 @@
+pyth_triplet_sum = 1000
+a = 1
+
+while(a <= pyth_triplet_sum/3):
+  b = a + 1
+  while(b <= pyth_triplet_sum/2):
+    c = pyth_triplet_sum - a - b
+    if(a*a + b*b == c*c):
+      print a*b*c
+    b = b + 1
+  a = a + 1
