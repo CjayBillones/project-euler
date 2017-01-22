a08aebe0392aebc0ba2e043cff87f81ef8013c8a
Cjay <billonesciprian08@gmail.com>
2017-01-23 01:50:56 +0800

    Add optimized alternative solution 2 in problem 7

--- python/project-euler-7.py
+++ python/project-euler-7.py
@@ -1,9 +1,7 @@
 from PrimalityTest import isPrime
 import time
 
-if __name__ == "__main__":
-  start_time = time.time()
-
+def solution_one():
   count = i = 0
 
   while count != 10001:
@@ -11,5 +9,22 @@ if __name__ == "__main__":
     if isPrime(i):
       count = count + 1
 
-  print i
+  return i
+
+def solution_two():
+  count = i = 1
+
+  while count != 10001:
+    i = i+2
+    if isPrime(i):
+      count = count + 1
+
+  return i
+
+if __name__ == "__main__":
+  start_time = time.time()
+
+  #print solution_one() # Execution Time: ~[0.086385, 0.092358] seconds
+  print solution_two() #Execution Time: ~[0.076524, 0.082660] seconds
+
   print("--- %s seconds ---" % (time.time() - start_time))
