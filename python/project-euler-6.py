fcd84174ac9429ad596fcf7c5f188820a957ff62
Cjay <billonesciprian08@gmail.com>
2017-01-22 20:50:40 +0800

    Add python solution for problem 6

---
+++ python/project-euler-6.py
@@ -0,0 +1,9 @@
+from Summation import summation_of_n
+from Summation import summation_of_n_squared
+
+n = 100
+
+sum_n = summation_of_n(n)
+sum_n_squared = summation_of_n_squared(n)
+
+print (sum_n**2) - sum_n_squared
