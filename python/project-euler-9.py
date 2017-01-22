df982d33424c615c806600bd64fb4562a7c4f578
Cjay <billonesciprian08@gmail.com>
2017-01-23 00:20:41 +0800

    Optimize problem 9 python solution

--- python/project-euler-9.py
+++ python/project-euler-9.py
@@ -1,17 +1,19 @@
 import time
 
-start_time = time.time()
+def PythagoreanTripletSum():
+  pyth_triplet_sum = 1000
+  a = 1
 
-pyth_triplet_sum = 1000
-a = 1
+  while(a <= pyth_triplet_sum/3):
+    b = a + 1
+    while(b <= pyth_triplet_sum/2):
+      c = pyth_triplet_sum - a - b
+      if(a*a + b*b == c*c):
+        return a*b*c  
+      b = b + 1
+    a = a + 1
 
-while(a <= pyth_triplet_sum/3):
-  b = a + 1
-  while(b <= pyth_triplet_sum/2):
-    c = pyth_triplet_sum - a - b
-    if(a*a + b*b == c*c):
-      print a*b*c
-    b = b + 1
-  a = a + 1
-
-print("--- %s seconds ---" % (time.time() - start_time))
+if __name__ == "__main__":
+  start_time = time.time()
+  print PythagoreanTripletSum()
+  print("--- %s seconds ---" % (time.time() - start_time))
