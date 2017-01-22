350d36336fcce02d4b3ab439308daf6cb78ea9a3
Cjay <billonesciprian08@gmail.com>
2017-01-23 01:21:19 +0800

    Add python solution for problem 4

---
+++ python/project-euler-4.py
@@ -0,0 +1,54 @@
+import time
+
+def isPalindrome(n):
+  return str(n) == str(n)[::-1]
+
+def solution_one():
+  palindromes = []
+
+  for i in range(100, 1000, 1):
+    for j in range(100, 1000, 1):
+      product = i*j
+      if isPalindrome(product):
+        palindromes.append(product)
+
+  return max(palindromes)
+
+def solution_two():
+  largest_palindrome = 0
+
+  for i in range(100, 1000, 1):
+    j = i
+    while j < 1000:
+      product = i*j
+      if isPalindrome(product) and product > largest_palindrome:
+        largest_palindrome = product
+      j = j+1
+
+  return largest_palindrome
+
+def solution_three():
+  largest_palindrome = 0
+  i = 999
+  while i >= 100:
+    j = i
+    while j >= 100:
+      product = i*j
+      if product <= largest_palindrome:
+        break
+      if isPalindrome(product):
+        largest_palindrome = product
+      j = j - 1
+    i = i - 1
+
+  return largest_palindrome
+
+
+if __name__ == "__main__":
+  start_time = time.time()
+
+  #print solution_one() # Execution Time: ~[0.393, 0.402] seconds
+  #print solution_two()  # Execution Time: ~[0.208, 0.215] seconds
+  print solution_three() # Execution Time: ~[0.004, 0.008] seconds
+
+  print "--- %s seconds ---" % (time.time() - start_time)
