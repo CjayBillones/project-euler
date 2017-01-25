8020661a9bd76f53e46e20097c434ee6444d5c63
Cjay <billonesciprian08@gmail.com>
2017-01-25 23:52:19 +0800

    Add python solution for problem 20

---
+++ python/project-euler-20.py
@@ -0,0 +1,12 @@
+from UtilityFunctions import conv_str_list_to_int
+import time
+import math
+
+def solution_one():
+  return sum(conv_str_list_to_int(list(str(math.factorial(100)))))
+
+if __name__ == "__main__":
+  start_time = time.time()
+
+  print solution_one() # Execution Time: ~[0.00012, 0.00032] seconds
+  print("--- %s seconds ---" % (time.time() - start_time))
