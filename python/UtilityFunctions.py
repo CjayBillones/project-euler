d249b0dc7b524fa33ced5312edd7065c332c86fc
Cjay <billonesciprian08@gmail.com>
2017-01-29 07:51:32 +0800

    Add num_to_words and remove_chars_from_string in Utility Functions
    * num_to_words converts numbers into english words
    * remove_chars_from_string removes specific characters from a given string

--- python/UtilityFunctions.py
+++ python/UtilityFunctions.py
@@ -66,5 +66,53 @@ def lcm_multiple(args):
 def dec_to_bin(n):
   return "{0:b}".format(n)
 
+# Shifts a string by n places
 def circular_shift_string(string, n):
   return string[n:] + string[:n]
+
+def remove_chars_from_string(string, chars):
+  sc = set(chars)
+  return ''.join([c for c in string if c not in sc])
+
+def num_to_words(num):
+    d = { 0 : 'zero', 1 : 'one', 2 : 'two', 3 : 'three', 4 : 'four', 5 : 'five',
+          6 : 'six', 7 : 'seven', 8 : 'eight', 9 : 'nine', 10 : 'ten',
+          11 : 'eleven', 12 : 'twelve', 13 : 'thirteen', 14 : 'fourteen',
+          15 : 'fifteen', 16 : 'sixteen', 17 : 'seventeen', 18 : 'eighteen',
+          19 : 'nineteen', 20 : 'twenty',
+          30 : 'thirty', 40 : 'forty', 50 : 'fifty', 60 : 'sixty',
+          70 : 'seventy', 80 : 'eighty', 90 : 'ninety' }
+    k = 1000
+    m = k * 1000
+    b = m * 1000
+    t = b * 1000
+
+    assert(0 <= num)
+
+    if (num < 20):
+        return d[num]
+
+    if (num < 100):
+        if num % 10 == 0: return d[num]
+        else: return d[num // 10 * 10] + '-' + d[num % 10]
+
+    if (num < k):
+        if num % 100 == 0: return d[num // 100] + ' hundred'
+        else: return d[num // 100] + ' hundred and ' + num_to_words(num % 100)
+
+    if (num < m):
+        if num % k == 0: return num_to_words(num // k) + ' thousand'
+        else: return num_to_words(num // k) + ' thousand, ' + num_to_words(num % k)
+
+    if (num < b):
+        if (num % m) == 0: return num_to_words(num // m) + ' million'
+        else: return num_to_words(num // m) + ' million, ' + num_to_words(num % m)
+
+    if (num < t):
+        if (num % b) == 0: return num_to_words(num // b) + ' billion'
+        else: return num_to_words(num // b) + ' billion, ' + num_to_words(num % b)
+
+    if (num % t == 0): return num_to_words(num // t) + ' trillion'
+    else: return num_to_words(num // t) + ' trillion, ' + num_to_words(num % t)
+
+    raise AssertionError('num is too large: %s' % str(num))
